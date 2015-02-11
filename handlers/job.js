c57d22b003571603c6faf225b79c28ff0100e4a7
diffsky <matthew.hadley@gmail.com>
2015-02-10 22:45:52 -0800

    add donut charts of run history

--- handlers/job.js
+++ handlers/job.js
@@ -6,6 +6,7 @@ var async = require('async');
 var db = require('../lib/db');
 var runner = require('../lib/runner');
 var formatter = require('../lib/json-formatter');
+var cache = require('../lib/cache');
 
 exports.act = function(request, reply) {
   var SLUM = request.params.slug + '-' + request.params.num;
@@ -17,6 +18,23 @@ exports.act = function(request, reply) {
 };
 
 exports.list = function(request, reply) {
+  var key = 'job/list';
+  var data = cache.get(key);
+
+  if (data && (data.generated > runner.lastrun)) {
+    if (request.query.json) {
+      return reply(formatter(data));
+    } else {
+      return reply.view('job/list', data);
+    }
+  }
+
+  var lengths = {
+    short: 5,
+    medium: 25,
+    long: 125
+  };
+
   var fns = [];
   db.job.find({}).sort({
     SLUG: 1
@@ -33,20 +51,48 @@ exports.list = function(request, reply) {
           }
         }).sort({
           NUM: -1
-        }).limit(5).exec(function(err, runs) {
+        }).limit(lengths.long).exec(function(err, runs) {
           job.runs = runs;
           cb(err, job);
         });
       });
     });
-    async.parallel(fns, function(err) {
+    async.parallel(fns, function(err, jobs) {
       if (err) {
         return reply(Hapi.error.internal(err));
       }
 
+      jobs.forEach(function(job) {
+        var totalRuns = job.runs.length;
+        job.stats = {
+          history: {}
+        };
+        Object.keys(lengths).forEach(function(length) {
+          job.stats.history[length] = {
+            PASS: 0,
+            FAIL: 0,
+            VOID: 0,
+            NONE: totalRuns > lengths[length] ? 0 : (lengths[length] - totalRuns)
+          };
+        });
+        job.runs.forEach(function(run, i) {
+          if (i < lengths.short) {
+            job.stats.history.short[run.TO.ST]++;
+          }
+          if (i < lengths.medium) {
+            job.stats.history.medium[run.TO.ST]++;
+          }
+          job.stats.history.long[run.TO.ST]++;
+        });
+        job.runs = job.runs.slice(0, 5);
+      });
+
       var data = {
+        generated: parseInt(+new Date() / 1000, 10),
         jobs: jobs
       };
+
+      cache.set(key, data);
 
       if (request.query.json) {
         return reply(formatter(data));
